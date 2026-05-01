88bae701658ff19b3800f125c6e75b5068a58302
Rahel Alemu <105115009+Rahel-Alemu@users.noreply.github.com>
2026-05-01 15:06:45 +0300

    Time: 0 ms (100.00%) | Memory: 12.3 MB (59.64%) - LeetSync

---
+++ 68-text-justification/text-justification.py
@@ -0,0 +1,18 @@
+class Solution:
+    def fullJustify(self, words, maxWidth):
+        res = []
+        cur_line = []
+        cur_len = 0
+        
+        for w in words:
+            if cur_len + len(w) + len(cur_line) > maxWidth:
+                for i in range(maxWidth - cur_len):
+                    cur_line[i % (len(cur_line) - 1 or 1)] += ' '
+                res.append("".join(cur_line))
+                cur_line, cur_len = [], 0
+            cur_line.append(w)
+            cur_len += len(w)
+            
+        last_line = " ".join(cur_line)
+        res.append(last_line + ' ' * (maxWidth - len(last_line)))
+        return res

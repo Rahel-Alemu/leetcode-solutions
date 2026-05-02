e006a5c343a308a29aa03dd7d233dc7f0e9f87a3
Rahel Alemu <105115009+Rahel-Alemu@users.noreply.github.com>
2026-05-02 07:14:18 +0300

    Time: 20 ms (39.11%) | Memory: 13 MB (38.22%) - LeetSync

---
+++ 87-scramble-string/scramble-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self):
+        self.memo = {}
+
+    def isScramble(self, s1, s2):
+        if (s1, s2) in self.memo:
+            return self.memo[(s1, s2)]
+        
+        if s1 == s2:
+            return True
+        if sorted(s1) != sorted(s2):
+            self.memo[(s1, s2)] = False
+            return False
+            
+        n = len(s1)
+        for i in range(1, n):
+            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) or \
+               (self.isScramble(s1[:i], s2[n-i:]) and self.isScramble(s1[i:], s2[:n-i])):
+                self.memo[(s1, s2)] = True
+                return True
+                
+        self.memo[(s1, s2)] = False
+        return False

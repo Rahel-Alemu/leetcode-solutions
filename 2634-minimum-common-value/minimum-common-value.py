f6172d3ab34a23bdae85561ada4455605003ad28
Rahel Alemu <105115009+Rahel-Alemu@users.noreply.github.com>
2026-05-19 05:31:20 +0300

    Time: 7 ms (97.46%) | Memory: 24.2 MB (83.47%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        i, j = 0, 0
+        n1, n2 = len(nums1), len(nums2)
+        
+        while i < n1 and j < n2:
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+                
+        return -1

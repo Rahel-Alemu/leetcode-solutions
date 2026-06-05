a29ad41c920518365381ca8cc57cf4468f935a85
Rahel Alemu <105115009+Rahel-Alemu@users.noreply.github.com>
2026-06-05 03:40:52 +0300

    Time: 1167 ms (100.00%) | Memory: 16.9 MB (66.67%) - LeetSync

---
+++ 4128-total-waviness-of-numbers-in-range-ii/total-waviness-of-numbers-in-range-ii.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def totalWaviness(self, num1, num2):
+        def solve(S):
+            memo = {}
+            def dfs(idx, prev2, prev1, is_less, is_started):
+                if idx == len(S):
+                    return (1, 0)
+                state = (idx, prev2, prev1, is_less, is_started)
+                if state in memo:
+                    return memo[state]
+                
+                limit = 9 if is_less else int(S[idx])
+                tot_count = 0
+                tot_wave = 0
+                
+                for d in range(limit + 1):
+                    next_less = is_less or (d < limit)
+                    
+                    if not is_started:
+                        if d == 0:
+                            c, w = dfs(idx + 1, -1, -1, next_less, False)
+                        else:
+                            c, w = dfs(idx + 1, -1, d, next_less, True)
+                    else:
+                        c, w = dfs(idx + 1, prev1, d, next_less, True)
+                        if prev2 != -1 and prev1 != -1:
+                            if (prev2 < prev1 and prev1 > d) or (prev2 > prev1 and prev1 < d):
+                                tot_wave += c
+                                
+                    tot_count += c
+                    tot_wave += w
+                                
+                memo[state] = (tot_count, tot_wave)
+                return memo[state]
+
+            return dfs(0, -1, -1, False, False)[1]
+
+        return solve(str(num2)) - solve(str(num1 - 1))
